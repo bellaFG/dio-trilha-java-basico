7d0630fbfb038af465679d4e30900a3847d08cc2
bellaFG <isabella.ferzales@gmail.com>
2024-01-24 10:10:53 -0300

    aprendendo operadores

---
+++ Operadores/Operadores/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        
+        numero = - numero;
+
+        System.out.println(- numero);
+
+        System.out.println(numero);
+
+    //para tranformar o número novamente em positivo devemos realizar uma multiplicação
+        numero = + numero * -1;
+
+        System.out.println(numero);
+    }
+    
+}
